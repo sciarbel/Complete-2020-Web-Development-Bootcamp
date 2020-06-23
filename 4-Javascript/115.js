335a92ffee5a018476a8c9efea9356346f429623
Sciarbel <sciarbel@gmail.com>
2020-06-23 15:11:48 -0300

    Added JS classes directory

---
+++ 4-Javascript/115.js
@@ -0,0 +1,21 @@
+// // Ex-1
+// var name = "Sciarbel";
+// // console.log(name.slice(0, 1));
+// // console.log(name.slice(1, 2));
+// // console.log(name.slice(0, 4));
+// // console.log(name.slice(1, 2));
+// // console.log(name.length);
+
+// Ex-2
+// var tweet = prompt("Compose your tweet:");
+// console.log("You typed " + tweet.length + " characters, and now have " + (140 - tweet.length) + " remaining.");
+
+// Ex-3
+// var tweet = prompt("Compose your tweet:");
+// var tweetCount = tweet.slice(0, 140);
+// alert(tweetCount);
+
+// Ex-4
+alert(prompt("Compose your tweet:").slice(0, 140));
+
+
