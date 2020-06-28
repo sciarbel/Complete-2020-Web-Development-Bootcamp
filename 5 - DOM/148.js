534c6d77023661877c1e695850b4600055256561
Sciarbel <sciarbel@gmail.com>
2020-06-28 00:44:03 -0300

    Added class 148

---
+++ 5 - DOM/148.js
@@ -0,0 +1,21 @@
+document.querySelector("ul").lastElementChild.innerHTML = "Sciarbel";
+
+document.getElementsByTagName("li")[2].style.color = "red";
+
+console.log(document.getElementsByTagName("li").length);
+
+document.getElementsByClassName("btn")[0].style.color = "orange";
+
+document.getElementById("title").style.color = "rgb(255, 190, 0)";
+
+document.getElementById("title").innerText = "goodbye";
+
+document.getElementById("title").innerHTML = "bye";
+
+document.getElementById("list").style.color = "orange";
+
+document.querySelector("li a").style.background = "pink";
+
+document.querySelectorAll("li.item")[1].style.background = "blue";
+
+document.querySelector("#list a").style.color = "red";
