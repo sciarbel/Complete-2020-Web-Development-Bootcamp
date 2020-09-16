96a976749159b30e81f19d8926d16f77aed32feb
Sciarbel <sciarbel@gmail.com>
2020-09-15 22:32:37 -0300

    Remade CLasses 115 and 116

--- 4-Javascript/115-117.js
+++ 4-Javascript/115-117.js
@@ -1,40 +1,17 @@
-// // Ex-1 115
+// CLass 115
+// var tweet = prompt("Compose your tweet: ");
+// console.log("You have written " + tweet.length + " characters. " + (140 - tweet.length) + " left.");
+
+// Class 116
+// Ex 1
 // var name = "Sciarbel";
-// // console.log(name.slice(0, 1));
-// // console.log(name.slice(1, 2));
-// // console.log(name.slice(0, 4));
-// // console.log(name.slice(1, 2));
-// // console.log(name.length);
+// console.log(name.slice(0, 1));
 
-// Ex-2
-// var tweet = prompt("Compose your tweet:");
-// console.log("You typed " + tweet.length + " characters, and now have " + (140 - tweet.length) + " remaining.");
+// Ex 2
+var tweet = prompt("Compose your tweet: ");
+console.log(tweet.slice(0, 140));
+console.log("You have written " + tweet.length + " characters. " + (140 - tweet.length) + " left.");
 
-// Ex-3
-// var tweet = prompt("Compose your tweet:");
-// var tweetCount = tweet.slice(0, 140);
-// alert(tweetCount);
 
-// Ex-4
-alert(prompt("Compose your tweet:").slice(0, 140));
 
-// // Ex-1 116
-// var name = "sciarbel";
-// console.log(name.toUpperCase());
 
-// // Ex-2
-// var name = "sciarbel";
-// name = name.toUpperCase();
-// console.log(name);
-
-// // Ex-3
-// var name = "sciarbel";
-// name = name.toLowerCase();
-// console.log(name);
-
-// Ex-4 Challenge
-var name = prompt("Type your name:");
-// var slc1 = name.slice(0, 1).toUpperCase();
-// var slc2 = name.slice(1, 20);
-// var upper = slc1.toUpperCase();
-console.log("Hello " + name.slice(0, 1).toUpperCase() + name.slice(1, name.lenght).toLowerCase());
