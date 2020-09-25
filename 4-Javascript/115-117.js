44ca5afbedd2926ca09fca0f397a031fd15470fd
Sciarbel <sciarbel@gmail.com>
2020-09-24 22:59:13 -0300

    Remade CLass 117

--- 4-Javascript/115-117.js
+++ 4-Javascript/115-117.js
@@ -2,16 +2,26 @@
 // var tweet = prompt("Compose your tweet: ");
 // console.log("You have written " + tweet.length + " characters. " + (140 - tweet.length) + " left.");
 
+
 // Class 116
 // Ex 1
 // var name = "Sciarbel";
 // console.log(name.slice(0, 1));
 
 // Ex 2
-var tweet = prompt("Compose your tweet: ");
-console.log(tweet.slice(0, 140));
-console.log("You have written " + tweet.length + " characters. " + (140 - tweet.length) + " left.");
+// var tweet = prompt("Compose your tweet: ");
+// console.log(tweet.slice(0, 140));
+// console.log("You have written " + tweet.length + " characters. " + (140 - tweet.length) + " left.");
 
 
+// Class 117
+// Ex 1
+var name = "sciarbel";
+name = name.toUpperCase();
+console.log(name);
+name = name.toLowerCase();
+console.log(name);
 
-
+// Ex2
+var name = prompt("What is your name?");
+console.log("Hello, " + (name.slice(0, 1)).toUpperCase() + name.slice(1));
