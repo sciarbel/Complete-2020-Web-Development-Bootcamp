2507cba9a4594ad248b9a18af5c7edd535125bf2
Sciarbel <sciarbel@gmail.com>
2020-09-30 23:36:15 -0300

    Updated Class 117

--- 4-Javascript/115-117.js
+++ 4-Javascript/115-117.js
@@ -24,4 +24,4 @@ console.log(name);
 
 // Ex2
 var name = prompt("What is your name?");
-console.log("Hello, " + (name.slice(0, 1)).toUpperCase() + name.slice(1));
+console.log("Hello, " + (name.slice(0, 1)).toUpperCase() + name.slice(1, name.length));
