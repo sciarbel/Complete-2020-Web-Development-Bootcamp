ec40cd563f37063a6399026783fc3c03bf015111
Sciarbel <sciarbel@gmail.com>
2020-10-24 19:45:54 -0300

    Remade Class 118

--- 4-Javascript/118-119.js
+++ 4-Javascript/118-119.js
@@ -1,4 +1,7 @@
+// Class 118
 // Ex-1
-var dogAge = prompt("How old is your dog?");
+var dogAge = prompt("What is your dog age?");
 var humanAge = ((dogAge - 2) * 4) + 21;
 console.log(humanAge);
+
+
